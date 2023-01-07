821096db4cb0404681f28c4db68b3c0762b49d61
redboard <ott.thomas68@gmail.com>
2023-01-07 18:45:10 +0100

    (+) add body parser

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const Discord = require('discord.js');
+const bodyParser = require("body-parser");
 require('dotenv').config();
 
 const app = express();
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-app.post("/log/:type", (req, res) => {
-    req.on('data', (data) => {
-        const type = req.params.type;
-        const message = "[" + type + "] " + data.toString();
-        const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-        channel.send(message).then(() => {
-            res.status(200).send('Message send');
-        }).catch((error) => {
-            res.status(500).send(error.message);
-        });
+app.post("/log/:app", async (req, res) => {
+    if (!req.params.hasOwnProperty('app') || !req.body.hasOwnProperty('message')) {
+        res.status(400).json({ msg: "Bad parameter" });
+        return;
+    }
+    const type = req.params.type;
+    const data = req.body.message;
+    const message = "[" + type + "] " + data.toString();
+    const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+
+    channel.send(message).then(() => {
+        res.status(200).send('Message send');
+    }).catch((error) => {
+        res.status(500).send(error.message);
     });
 });
 
