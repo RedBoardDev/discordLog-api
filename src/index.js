608fb63d2997566aff6eadec8c3b5a915ba5ce19
redboard <ott.thomas68@gmail.com>
2023-01-07 15:52:01 +0100

    (+) API with log route

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const app = express();
+const client = new Discord.Client();
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+app.post('/log/:type', (req, res) => {
+    req.on('data', (data) => {
+        const type = req.params.type;
+        const message = "[" + type + "] " + data.toString();
+        const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+
+        channel.send(message).then(() => {
+            res.status(200).send('Message send');
+        }).catch((error) => {
+            res.status(500).send(error.message);
+        });
+    });
+});
+
+const port = process.env.API_PORT;
+app.listen(port, () => {
+    console.log(`API launch at ${port}`);
+});
